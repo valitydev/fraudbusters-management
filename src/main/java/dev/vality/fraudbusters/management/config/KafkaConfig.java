fc915a9fc1cff8396483f7f2a87be715cba658ba
malkoas <alien.life.form.1986@gmail.com>
2022-10-20 13:59:52 +0300

    TD-390: add iam support + kafka config

--- src/main/java/dev/vality/fraudbusters/management/config/KafkaConfig.java
+++ src/main/java/dev/vality/fraudbusters/management/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import dev.vality.fraudbusters.management.serializer.EventDeserializer;
 import dev.vality.fraudbusters.management.serializer.FraudDataCandidateDeserializer;
 import dev.vality.fraudbusters.management.serializer.ReferenceInfoDeserializer;
 import dev.vality.kafka.common.serialization.ThriftSerializer;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -18,6 +19,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.thrift.TBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -33,38 +35,20 @@ import java.util.Map;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_RECORDS_CONFIG;
 
 @Configuration
+@RequiredArgsConstructor
 @SuppressWarnings("LineLength")
 public class KafkaConfig {
 
-    public static final String PKCS_12 = "PKCS12";
-    private static final String GROUP_ID = "FraudBusterListener";
-    private static final String EARLIEST = "earliest";
-    @Value("${kafka.bootstrap-servers}")
-    private String bootstrapServers;
-    @Value("${kafka.ssl.server-password}")
-    private String serverStorePassword;
-    @Value("${kafka.ssl.server-keystore-location}")
-    private String serverStoreCertPath;
-    @Value("${kafka.ssl.keystore-password}")
-    private String keyStorePassword;
-    @Value("${kafka.ssl.key-password}")
-    private String keyPassword;
-    @Value("${kafka.ssl.keystore-location}")
-    private String clientStoreCertPath;
-    @Value("${kafka.ssl.enable}")
-    private boolean kafkaSslEnable;
     @Value("${kafka.consumer-group.wb-list}")
     private String consumerGroupWbList;
 
+    private final KafkaProperties kafkaProperties;
+
     @Bean
     public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, EARLIEST);
         props.put(MAX_POLL_RECORDS_CONFIG, 1);
-        sslConfigure(props);
         return props;
     }
 
@@ -178,25 +162,10 @@ public class KafkaConfig {
 
     @Bean
     public ProducerFactory<String, TBase> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        Map<String, Object> configProps = kafkaProperties.buildProducerProperties();
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ThriftSerializer.class);
-        sslConfigure(configProps);
         return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    private void sslConfigure(Map<String, Object> configProps) {
-        if (kafkaSslEnable) {
-            configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
-            configProps.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, new File(serverStoreCertPath).getAbsolutePath());
-            configProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, serverStorePassword);
-            configProps.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, PKCS_12);
-            configProps.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, PKCS_12);
-            configProps.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, new File(clientStoreCertPath).getAbsolutePath());
-            configProps.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, keyStorePassword);
-            configProps.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, keyPassword);
-        }
     }
 
     @Bean

de242be72dd00c7b94f9ca6eed1f9d5353f2d34f
struga <kennny007@yandex.ru>
2024-08-09 15:24:17 +0300

    Up woody (#70)

---
+++ src/main/java/dev/vality/fraudbusters/management/config/WebConfig.java
@@ -0,0 +1,66 @@
+package dev.vality.fraudbusters.management.config;
+
+import dev.vality.woody.api.flow.WFlow;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@SuppressWarnings({"ParameterName", "LocalVariableName"})
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*");
+            }
+        };
+    }
+
+    @Bean
+    public FilterRegistrationBean woodyFilter() {
+        WFlow woodyFlow = new WFlow();
+        Filter filter = new OncePerRequestFilter() {
+
+            @Override
+            protected void doFilterInternal(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain filterChain) {
+                woodyFlow.createServiceFork(
+                                () -> {
+                                    try {
+                                        filterChain.doFilter(request, response);
+                                    } catch (IOException | ServletException e) {
+                                        sneakyThrow(e);
+                                    }
+                                }
+                        )
+                        .run();
+            }
+
+            private <E extends Throwable, T> T sneakyThrow(Throwable t) throws E {
+                throw (E) t;
+            }
+        };
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.setOrder(-50);
+        filterRegistrationBean.setName("woodyFilter");
+        filterRegistrationBean.addUrlPatterns("*");
+        return filterRegistrationBean;
+    }
+}

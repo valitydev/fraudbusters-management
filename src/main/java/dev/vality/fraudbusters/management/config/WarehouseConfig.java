a33f5dd0fe9c7d379585ce434270f7a6b5548d43
kostyastruga <koztyastruga@gmail.com>
2024-08-12 18:02:10 +0300

    Up woody

--- src/main/java/dev/vality/fraudbusters/management/config/WarehouseConfig.java
+++ src/main/java/dev/vality/fraudbusters/management/config/WarehouseConfig.java
@@ -1,17 +1,25 @@
 package dev.vality.fraudbusters.management.config;
 
+import dev.vality.fraudbusters.management.config.properties.OtelProperties;
 import dev.vality.fraudbusters.warehouse.QueryServiceSrv;
 import dev.vality.woody.thrift.impl.http.THSpawnClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class WarehouseConfig {
 
+    @Autowired
+    private OtelProperties otelProperties;
     @Bean
     public QueryServiceSrv.Iface bouncerClient(@Value("${service.warehouse.url}") Resource resource,
                                                @Value("${service.warehouse.networkTimeout}") int networkTimeout)
@@ -19,6 +27,7 @@ public class WarehouseConfig {
         return new THSpawnClientBuilder()
                 .withNetworkTimeout(networkTimeout)
                 .withAddress(resource.getURI())
+                .withOtelResource(otelProperties.getResource())
                 .withLogEnabled(true)
                 .build(QueryServiceSrv.Iface.class);
     }
